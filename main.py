357eeb8c93111ecca1a4f5c505c41cbf1e662444
EC2 Default User <ec2-user@ip-172-31-17-83.us-east-2.compute.internal>
2021-09-01 00:30:54 +0000

    inital commit

---
+++ main.py
@@ -0,0 +1,13 @@
+  
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Duke"}
+    
+    
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')

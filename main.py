8560d5d3c9607ee9d94f384a7090f6731c06e7cb
Shufan Xia <sx78@duke.edu>
2021-09-01 01:40:45 +0000

    add add()

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ async def add(num1: int, num2: int):
     total = num1 + num2
     return {"total": total}
     
+@app.get("/greeting/{name}")
+async def greeting(name: str):
+    return {"greeting": name}
+    
     
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')

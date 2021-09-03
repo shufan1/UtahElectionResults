93f8a0d7245bb254e47bba107fbd32e38f4ae9fb
Shufan Xia <sx78@duke.edu>
2021-09-03 20:07:42 +0000

    fix regex

--- scraper.py
+++ scraper.py
@@ -28,14 +28,6 @@ def build_election_list(html):
     pattern2 = r"\d{4}|[A-Za-z]+"
     election_name  = list(map(lambda s: " ".join(re.findall(pattern2,s)),election_list))
     election_name = list(map(lambda s: s[:5]+type[s[5:]]+" Election",election_name))
-    # url_list = extract_url()
-    # #pattern = r"\d{4}[A-Za-z ]+?(?:General|Primary)"
-    # pattern = r"(?<!Default\/)(?<=\/)\d{4}[-|\%20]*?[^\d]*?(?:General|Primar|Gen|Pri)"
-    # election_list = list(map(lambda url: re.findall(pattern,url)[0],url_list))
-    # pattern2 = r"\d{4}|[A-Za-z]+"
-    # election_name  = list(map(lambda s: " ".join(re.findall(pattern2,s)),election_list))
-    # election_name = list(map(lambda s: s[:5]+type[s[5:]]+" Election",election_name))
-    
     return election_name
 
 def make_url_dict(html):
@@ -59,17 +51,24 @@ url2= "https://voteinfo.utah.gov/wp-content/uploads/sites/42/2021/02/2020-Primar
 def get_juidicial(url):
     try:
         df = pd.read_excel(url,sheet_name="Judicial") 
-        candidate_position = df.columns[1::2]
+        pattern = r"^(?!\d{4})(?:(?!Unnamed).)+?.+$"
+        candidate_position = list( map(lambda x: re.match(pattern,x),df))
+        candidate_position =  [i[0] for i in candidate_position if i is not None]
         df = list(map(lambda s:match(s),candidate_position))
         df = np.array(df)
         name = df[:,0]
         position = df[:,1]
-        
         return (pd.DataFrame({'Candidate':name,'Office':position}))
         
     except:
          return ("No judicial offices were elected in this election")
         
 def match(name_position):
-    match = re.split(r'\s{5,}', name_position)
+    match = re.split(r'\s{5,}|,', name_position)
     return match
+    
+    
+#url3 = "https://elections.utah.gov/Media/Default/2018%20Election/2018%20General%20Election%20Canvass.xlsx"
+
+
+url = "https://elections.utah.gov/Media/Default/2016%20Election/2016%20Primary%20Election%20State%20Canvass.xlsx"
